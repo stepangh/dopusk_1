49a3cec579c854ff3cabd5dff4599e1de2e5af7b
stepangh <44944029+stepangh@users.noreply.github.com>
2020-03-25 13:43:03 +0300

    Update A_func.m

--- A_func.m
+++ A_func.m
@@ -2,6 +2,7 @@ function A = A_func(codes)
 
 A = zeros(1, length(codes(1, :)) + 1);
 
+% Формирование множества кодовых слов веса i
 for i = 1 : length(codes(:,1))
     weigth = sum(codes(i, :));
     A(1, weigth + 1) = A(1, weigth + 1) + 1;    
